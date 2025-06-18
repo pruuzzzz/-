c5dc7f57482aa1d780afb41a137b852439af6427
DHK <aporist@outlook.com>
2025-06-18 14:26:09 +0900

    초기 커밋.

---
+++ MauiApp4/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace MauiApp4
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+        private void OnTemperatureChanged(object sender, ValueChangedEventArgs e)
+        {
+            int temp = (int)e.NewValue;
+            TemperatureLabel.Text = $"현재 온도: {temp} K";
+
+            // 여기서 추가로 물리 계산이나 시뮬레이션에 반영해도 됨
+            // 예: UpdateParticleSpeed(temp);
+        }
+    }
+
+}
