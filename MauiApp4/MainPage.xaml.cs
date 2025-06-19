3f80e87fbc544d5323c5dec579a89dba343589dd
DHK <aporist@outlook.com>
2025-06-19 10:18:00 +0900

    .1.1

--- MauiApp4/MainPage.xaml.cs
+++ MauiApp4/MainPage.xaml.cs
@@ -9,17 +9,7 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+        
         private void OnTemperatureChanged(object sender, ValueChangedEventArgs e)
         {
             int temp = (int)e.NewValue;
@@ -28,6 +18,52 @@
             // 여기서 추가로 물리 계산이나 시뮬레이션에 반영해도 됨
             // 예: UpdateParticleSpeed(temp);
         }
+        private void OnValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            int val = (int)e.NewValue;
+            ValueLabel.Text = $"현재 부피: {val} m^3";
+
+            // 여기서 추가로 물리 계산이나 시뮬레이션에 반영해도 됨
+            // 예: UpdateParticleSpeed(temp);
+        }
+        private void OnPressureChanged(object sender, ValueChangedEventArgs e)
+        {
+            int press = (int)e.NewValue;
+            PressureLabel.Text = $"현재 압력: {press} hpa";
+
+            // 여기서 추가로 물리 계산이나 시뮬레이션에 반영해도 됨
+            // 예: UpdateParticleSpeed(temp);
+        }
+        private double n = 1.0; // 기본값
+
+        private void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            // 입력값을 int로 변환
+            if (int.TryParse(e.NewTextValue, out int result))
+            {
+                MyResultLabel.Text = $"입력한 숫자: {result}";
+            }
+            else
+            {
+                MyResultLabel.Text = "정수를 입력하세요!";
+            }
+        }
+        private void OnCheckBoxChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (sender is CheckBox selected && selected.IsChecked)
+            {
+                // 모든 체크박스를 배열로 묶고
+                var checkBoxes = new[] { Chk1, Chk2, Chk3, Chk4 };
+
+                foreach (var cb in checkBoxes)
+                {
+                    // 선택된 체크박스만 제외하고 전부 false로
+                    if (cb != selected)
+                        cb.IsChecked = false;
+                }
+            }
+        }
+
     }
 
 }
